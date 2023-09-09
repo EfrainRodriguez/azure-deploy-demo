d8eb2ebfbac5792bd225a2efd464098991fe8064
EfrainRodriguez <efrainrg92@gmail.com>
2023-09-09 18:53:58 -0500

    adding react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+      <Routes>
+        <Route path="/test" element={<div>test page</div>} />
+      </Routes>
+      <Routes>
+        <Route path="/login" element={<div>login page</div>} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
